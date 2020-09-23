fd99c49650d14011196a755e7967571ec2434a85
Pedro Perez <pperezm@tec.mx>
2020-09-23 18:09:23 -0500

    initial push

---
+++ list.h
@@ -0,0 +1,258 @@
+#ifndef List_H
+#define List_H
+
+#include <string>
+#include <sstream>
+#include "exception.h"
+
+typedef unsigned int uint;
+
+template <class T> class List;
+
+/* Definition of the Node class */
+template <class T>
+class Node {
+private:
+	Node(T);
+	Node(T, Node<T>*);
+
+	T	    value;
+	Node<T> *next;
+
+	friend class List<T>;
+};
+
+template <class T>
+Node<T>::Node(T val) : value(val), next(0) {
+}
+
+template <class T>
+Node<T>::Node(T val, Node* nxt) : value(val), next(nxt) {
+}
+
+/* Definition of the List class */
+template <class T>
+class List {
+private:
+	Node<T> *head;
+	uint 	size;
+
+public:
+	List();
+	~List();
+
+	uint  length() const;
+	bool empty() const;
+	bool contains(T) const;
+	void clear();
+	std::string toString() const;
+
+	T    front() const;
+	T    last() const;
+	T    get(uint) const;
+
+	void push_front(T);
+	void push_back(T);
+	void insert_at(T, unint);
+
+	T    pop_front();
+	T 	 pop_back();
+	T    remove_at(uint);
+
+	long int  index_of(T) const;
+};
+
+template <class T>
+List<T>::List() :head(0), size(0) {
+}
+
+template <class T>
+List<T>::~List() {
+    clear();
+}
+
+template <class T>
+bool List<T>::empty() const {
+	return (head == 0);
+}
+
+template <class T>
+uint List<T>::length() const {
+	return size;
+}
+
+template <class T>
+bool List<T>::contains(T val) const {
+	Node<T> *p;
+
+	p = head;
+	while (p != 0) {
+		if(p->value == val) {
+			return true;
+		}
+		p = p->next;
+	}
+	return false;
+}
+
+template <class T>
+void List<T>::clear() {
+	Node<T> *p, *q;
+
+	p = head;
+	while (p != 0){
+		q = p->next;
+		delete p;
+		p = q;
+	}
+
+	head = 0;
+	size = 0;
+}
+
+template <class T>
+std::string List<T>::toString() const {
+	std::stringstream aux;
+	Node<T> *p;
+
+	p = head;
+	aux << "[";
+	while (p != 0) {
+		aux << p->value;
+		if (p->next != 0) {
+			aux << ", ";
+		}
+		p = p->next;
+	}
+	aux << "]";
+	return aux.str();
+}
+
+template <class T>
+T List<T>::front() const {
+	if (empty()) {
+		throw NoSuchElement();
+	}
+
+	return head->value;
+}
+
+template <class T>
+T List<T>::last() const {
+	Node<T> *p;
+
+	if (empty()) {
+		throw NoSuchElement();
+	}
+
+	p = head;
+	while (p->next != null) {
+		p = p->next;
+	}
+	return p->value;
+}
+
+template <class T>
+T List<T>::get(uint index) const {
+	T aux;
+
+	// TO DO
+	return aux;
+}
+
+template <class T>
+void List<T>::push_front(T val) {
+	Node<T> *q;
+
+	q = new Node<T>(val);
+	q->next = head;
+	head = q;
+	size++;
+}
+
+template <class T>
+void List<T>::push_back(T val) {
+	Node<T> *p, *q;
+
+	if (empty()) {
+		push_front(val);
+		return;
+	}
+
+	p = head;
+	while (p->next != 0) {
+		p = p->next;
+	}
+
+	q = new Node<T>(val);
+	q->next = p->next;
+	p->next = q;
+	size++;
+}
+
+template <class T>
+void List<T>::insert_at(T val, uint index) {
+}
+
+template <class T>
+T List<T>::pop_front() {
+	T val;
+	Node<T> *p;
+
+	if (empty()) {
+		throw NoSuchElement();
+	}
+
+	p = head;
+
+	head = p->next;
+	val = p->value;
+
+	delete p;
+	size--;
+	return val;
+}
+
+template <class T>
+T List<T>::pop_back() {
+	Node<T> *p, *q;
+	T val;
+
+	if (empty()) {
+		throw NoSuchElement();
+	}
+
+	if (size == 1) {
+		return pop_front();
+	}
+
+	q = 0;
+	p = head;
+	while (p->next != 0) {
+		q = p;
+		p = p->next;
+	}
+
+	q->next = p->next;
+	val = p->value;
+
+	delete p;
+	size--;
+
+	return val;
+}
+
+template <class T>
+T List<T>::remove_at(uint index) const {
+	T aux;
+	// TO DO
+	return aux;
+}
+
+template <class T>
+long int List<T>::index_of(T val) const {
+	// TO DO
+	return -1;
+}
+
+#endif /* NodeEDList_H_ */
